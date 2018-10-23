6bce82b8b5e6277ec4065c196e0d5b6f5e038640
KenavR <kenavr@gmail.com>
2018-10-23 13:58:29 +0200

    Removed generated files

--- app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Other_1 = require("./Other");
-var x = new Other_1.Other();
-x.setcar = "Mario";
-document.write(x.getcar);
-//# sourceMappingURL=app.js.map
